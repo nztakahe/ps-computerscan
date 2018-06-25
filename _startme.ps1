903cad13eb8c2013bd3b0b6e4a6a9c427a888cd2
Michael Menzies <michael@dwarfstar.co.nz>
2018-06-26 06:08:37 +1200

    improve handling of unresolved ping. Start using my ScanCode in examples and improving output.

--- _startme.ps1
+++ _startme.ps1
@@ -2,12 +2,18 @@
 [System.Management.Automation.PathInfo] $startdir = Get-Location
 
 [String] $scriptroot = $PSScriptRoot
-Set-Location -Path $scriptroot
-Import-Module .\invokeallV2.5.ps1
-Import-Module .\getuptime.ps1
 
-Get-Content -Path .\computers.txt | Invoke-All {Get-Uptime} -WaitTimeOut 1000| Format-Table
+#Add Function Librarys
+. $PSScriptRoot\invokeallV2.5.ps1
+. $PSScriptRoot\ComputerScan.ps1
 
+#. $PSScriptRoot\getuptime.ps1
+
+Get-Content -Path $PSScriptRoot\computers.txt | Invoke-All {Get-ComputerScan} -WaitTimeOut 1000| Format-Table
+
+
+remove-item function:get-computerscan
+remove-item function:invoke-all
 
 #Change to Starting Directory
 #Set-Location -Path $startdir.Path
